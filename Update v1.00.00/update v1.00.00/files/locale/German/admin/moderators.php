9fe1f15b1c5f12274c223b2c96c14f2b6630d1bf
chrome511 <chrome@chrome-fusion.de>
2019-12-01 13:56:48 +0100

    Uploaded to GitHub

---
+++ Update v1.00.00/update v1.00.00/files/locale/German/admin/moderators.php
@@ -0,0 +1,69 @@
+<?php
+/*-------------------------------------------------------+
+| Author: Patrick Kruse (Chrome)
+| Filename: moderators.php (locale)
+| Based on administrators.php (locale)
++--------------------------------------------------------+
+| Modified by Patrick Kruse (Chrome)
+| Moderator function by Patrick Kruse (Chrome)
+| Copyright by Patrick Kruse (Chrome)
+| http://chrome-fusion.de
++--------------------------------------------------------*/
+
+// Moderator Nachrichten
+$locale['400'] = "Der Moderator wurde hinzugef&uuml;gt.";
+$locale['401'] = "Der Moderator wurde aktualisiert.";
+$locale['402'] = "Der Moderator wurde gel&ouml;scht.";
+
+// Suche / Moderator hinzufuegen
+$locale['410'] = "Moderator hinzuf&uuml;gen";
+$locale['411'] = "Benutze das folgende Formular, um Benutzer zu finden, die du zu einem Moderator machen m&ouml;chtest.";
+$locale['412'] = "Benutzer ID";
+$locale['413'] = "Benutzername";
+$locale['414'] = "Suche starten";
+$locale['415'] = "Benutzer globale Moderator Rechte geben<br>(Benutzer, Blacklist, Einsendungen, Kommentare)";
+$locale['416'] = "Benutzer als Seiten Moderator hinzuf&uuml;gen<br>(Benutzer, Blacklist, Einsendungen, Kommentare,<br>Moderatoren hinzuf&uuml;gen, Moderatoren Passwort reset)";
+$locale['417'] = "Moderator hinzuf&uuml;gen";
+$locale['418'] = "Es wurden keine Benutzer gefunden, die deiner Suchanfrage entsprechen.";
+$locale['419'] = "Erneut suchen";
+
+// Aktuelle Moderatoren
+$locale['420'] = "Moderatoren";
+$locale['421'] = "Benutzername";
+$locale['422'] = "Moderatorlevel";
+$locale['423'] = "Optionen";
+//$locale['424'] = "Hauptadmin";
+$locale['425'] = "Keine Rechte vergeben";
+$locale['426'] = "Bearbeiten";
+$locale['427'] = "L&ouml;schen";
+
+// Moderatorrechte bearbeiten
+$locale['440'] = "Moderatorrechte bearbeiten";
+$locale['441'] = "Inhalt";
+$locale['442'] = "Benutzer";
+$locale['443'] = "System";
+$locale['444'] = "Infusions";
+$locale['445'] = "Alle markieren";
+$locale['446'] = "Alle demarkieren";
+$locale['447'] = "Moderator Passwort:";
+$locale['447a'] = "Admin Passwort:";
+$locale['448'] = "Moderatorrechte speichern";
+$locale['449'] = "Einstellungen";
+$locale['450'] = "Alle sicheren Rechte markieren";
+
+// Javascript Alert Nachrichten
+$locale['460'] = "Willst du diesen Moderator wirklich löschen?";
+$locale['461'] = "Willst du diesen Moderator wirklich hinzufügen?";
+$locale['462'] = "Einem Benutzer diese Rechte zu geben ist ein Sicherheitsrisiko!";
+
+// Administrator entfernen
+$locale['470'] = "Moderator l&ouml;schen";
+$locale['471'] = "Wenn du diesen Moderator l&ouml;schen m&ouml;chtest, musst du dein Moderator Passwort angeben:";
+$locale['472'] = "Weiter";
+$locale['473'] = "Abbrechen";
+
+// BITTE AB HIER NICHT MEHR BEARBEITEN !!
+#$locale['460'] = utf8_encode($locale['460']);
+#$locale['461'] = utf8_encode($locale['461']);
+
+?>
---
+++ files/themes/templates/setup/dist/index.php
@@ -0,0 +1,5 @@
+<?php
+
+/** Bin nur eine Leere Index **/
+
+?>

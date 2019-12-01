e500f71a89b25b28568d7995aa1ef50c4ef6d8b7
chrome511 <chrome@chrome-fusion.de>
2019-12-01 14:02:25 +0100

    Uploaded to GitHub

---
+++ Update v1.00.00/update v1.00.00/files/locale/English/bbcodes/search.php
@@ -0,0 +1,23 @@
+<?php
+/*-------------------------------------------------------+
+| Modified by Patrick Kruse (Chrome)
+| Copyright by Patrick Kruse (Chrome)
+| http://chrome-fusion.de
++--------------------------------------------------------*/
+$locale['bb_search_description'] = "Searching for given text on current site";
+$locale['bb_search_usage'] = "Text to search";
+$locale['bb_search_prefix'] = "Search for ";
+$locale['bb_search_suffix'] = " on ";
+$locale['bb_search_where'] = "Where";
+
+$locale['bb_search_all'] = "Entire website";
+$locale['bb_search_forums'] = "In forum posts";
+$locale['bb_search_articles'] = "In articles";
+$locale['bb_search_members'] = "For members";
+$locale['bb_search_weblinks'] = "In Web links";
+$locale['bb_search_photos'] = "In the photo gallery";
+$locale['bb_search_custompages'] = "In content pages";
+$locale['bb_search_faqs'] = "In the FAQ";
+$locale['bb_search_news'] = "In news articles";
+$locale['bb_search_downloads'] = "In downloads";
+?>
